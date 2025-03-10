63db4b720c3a490d99140ca9f0e70ef5f716a495
Rizqie Ahmad <rizqie@rectmedia.com>
2025-03-10 14:49:48 +0700

    menambahkan implementasi dengan state-based construction

---
+++ tpmodul4_103022300118_2/DoorMachine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class DoorMachine
+{
+    public enum DoorState
+    {
+        Terkunci,
+        Terbuka
+    }
+
+    private DoorState currentState;
+
+    public DoorMachine()
+    {
+        currentState = DoorState.Terkunci;
+        Console.WriteLine("Pintu terkunci");
+    }
+
+    public void BukaPintu()
+    {
+        if (currentState == DoorState.Terkunci)
+        {
+            currentState = DoorState.Terbuka;
+            Console.WriteLine("Pintu tidak terkunci");
+        }
+        else
+        {
+            Console.WriteLine("Pintu sudah terbuka!");
+        }
+    }
+
+    public void KunciPintu()
+    {
+        if (currentState == DoorState.Terbuka)
+        {
+            currentState = DoorState.Terkunci;
+            Console.WriteLine("Pintu terkunci");
+        }
+        else
+        {
+            Console.WriteLine("Pintu sudah terkunci!");
+        }
+    }
+}
---
+++ tpmodul4_103022300118_2/MainKode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        DoorMachine pintu = new DoorMachine();
+
+        while (true) 
+        {
+            Console.WriteLine("\nKetik 'buka' untuk membuka pintu, 'kunci' untuk mengunci pintu, atau 'exit' untuk keluar:");
+            Console.Write("Masukkan perintah: ");
+            string input = Console.ReadLine().ToLower();
+
+            if (input == "buka")
+            {
+                pintu.BukaPintu();
+            }
+            else if (input == "kunci")
+            {
+                pintu.KunciPintu();
+            }
+            else if (input == "exit")
+            {
+                Console.WriteLine("Program selesai.");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Perintah tidak dikenal. Silakan ketik 'buka', 'kunci', atau 'exit'.");
+            }
+        }
+    }
+}
